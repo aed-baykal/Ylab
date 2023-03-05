b8767dccca0a947322d3d565683be02a8acdbfeb
Andrey Dubovitskiy <aed_baykal@mail.ru>
2023-03-05 15:07:24 +0300

    Задача Pell

--- src/Pell.java
+++ src/Pell.java
@@ -1,3 +1,22 @@
-public class Pell {
+import java.util.Scanner;
 
+public class Pell {
+    public static void main(String[] args) throws Exception {
+        try (Scanner scanner = new Scanner(System.in)) {
+            int n = scanner.nextInt();
+            if (n == 0) System.out.println("0");
+            else if (n == 1) System.out.println("1");
+            else {
+                int pellNumberN = 1;
+                int pellNumberNMinus1;
+                int pellNumberNMinus2 = 0;
+                for (int i = 1; i < n; i++) {
+                    pellNumberNMinus1 = pellNumberN;
+                    pellNumberN = 2 * pellNumberNMinus1 + pellNumberNMinus2;
+                    pellNumberNMinus2 = pellNumberNMinus1;
+                }
+                System.out.println(pellNumberN);
+            }
+        }
+    }
 }
