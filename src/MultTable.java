26119fd4f537492c1c19a64d19f2be08d0b3e371
Andrey Dubovitskiy <aed_baykal@mail.ru>
2023-03-05 15:18:18 +0300

    Задача MultTable

--- src/MultTable.java
+++ src/MultTable.java
@@ -1,3 +1,9 @@
 public class MultTable {
-
+    public static void main(String[] args) throws Exception {
+        for (int multiplier1 = 1; multiplier1 <= 9; multiplier1++) {
+            for (int multiplier2 = 1; multiplier2 <= 9 ; multiplier2++) {
+                System.out.println(multiplier1 + " x " + multiplier2 + " = " + multiplier1*multiplier2);
+            }
+        }
+    }
 }
