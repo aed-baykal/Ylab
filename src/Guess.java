42e4cb49912889d5f4996c86a3fbde69f4d8fb89
Andrey Dubovitskiy <aed_baykal@mail.ru>
2023-03-06 12:53:22 +0300

    fix задача Guess

--- src/Guess.java
+++ src/Guess.java
@@ -7,20 +7,21 @@ public class Guess {
         int maxAttempts = 10; // здесь задается количество попыток
         int enteredNumber;
         int attempt = 1;
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Я загадал число. У тебя " + maxAttempts + " попыток угадать.");
-        while (attempt <= 10) {
-            System.out.print("Введи число: ");
-            enteredNumber = scanner.nextInt();
-            if (enteredNumber == number) {
-                System.out.println("Ты угадал с " + attempt + " попытки");
-                break;
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Я загадал число от 1 до 99. У тебя " + maxAttempts + " попыток угадать.");
+            while (attempt <= 10) {
+                System.out.print("Введи число: ");
+                enteredNumber = scanner.nextInt();
+                if (enteredNumber == number) {
+                    System.out.println("Ты угадал с " + attempt + " попытки");
+                    break;
+                } else if (enteredNumber > number)
+                    System.out.println("Мое число меньше! У тебя осталось " + (maxAttempts - attempt) + " попыток");
+                else System.out.println("Мое число больше! У тебя осталось " + (maxAttempts - attempt) + " попыток");
+                attempt++;
             }
-            else if (enteredNumber > number) System.out.println("Мое число меньше! У тебя осталось " + (maxAttempts - attempt) + " попыток");
-            else System.out.println("Мое число больше! У тебя осталось " + (maxAttempts - attempt) + " попыток");
-            attempt++;
+            if (attempt > maxAttempts) System.out.println("Ты не угадал");
         }
-        if (attempt == 11) System.out.println("Ты не угадал");
     }
 }
