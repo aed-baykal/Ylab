b71cbc35dadcadc43a44d09372a130eaca038b23
Andrey Dubovitskiy <aed_baykal@mail.ru>
2023-03-06 12:41:27 +0300

    fix задача Pell

--- src/Pell.java
+++ src/Pell.java
@@ -3,20 +3,23 @@ import java.util.Scanner;
 public class Pell {
     public static void main(String[] args) throws Exception {
         try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Введите индекс числа Пелля от 0 до 30: ");
             int n = scanner.nextInt();
-            if (n == 0) System.out.println("0");
-            else if (n == 1) System.out.println("1");
-            else {
-                int pellNumberN = 1;
-                int pellNumberNMinus1;
-                int pellNumberNMinus2 = 0;
-                for (int i = 1; i < n; i++) {
-                    pellNumberNMinus1 = pellNumberN;
-                    pellNumberN = 2 * pellNumberNMinus1 + pellNumberNMinus2;
-                    pellNumberNMinus2 = pellNumberNMinus1;
+            if ((n >= 0) && (n <= 30)){
+                if (n == 0) System.out.println("Число Пелля = 0");
+                else if (n == 1) System.out.println("Число Пелля = 1");
+                else {
+                    int pellNumberN = 1;
+                    int pellNumberNMinus1;
+                    int pellNumberNMinus2 = 0;
+                    for (int i = 1; i < n; i++) {
+                        pellNumberNMinus1 = pellNumberN;
+                        pellNumberN = 2 * pellNumberNMinus1 + pellNumberNMinus2;
+                        pellNumberNMinus2 = pellNumberNMinus1;
+                    }
+                    System.out.println("Число Пелля = " + pellNumberN);
                 }
-                System.out.println(pellNumberN);
-            }
+            } else System.out.println("Введенное число за пределами 0 - 30");
         }
     }
 }
